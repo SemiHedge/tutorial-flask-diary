8dd9608f2c34a7c110c789ec1ccd0af217154b99
프로그래머스 교육팀 <soorte214@gmail.com>
2022-05-26 21:04:22 +0900

    OneToMany, ForeignKey
    - One(User)ToMany(Note)
    - db.ForeignKey('클래스.속성')
    - db.relationship('클래스')

--- website/models.py
+++ website/models.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     nickname = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200))
+    notes = db.relationship('Note')
 
 
 # define Note Model
@@ -16,3 +17,4 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(2000))
     datetime = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
